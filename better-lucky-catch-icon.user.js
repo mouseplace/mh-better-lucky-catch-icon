0756b907256eb5fbcc7e9d8f563500998f2d0d8e
Brad Parbs <brad@bradparbs.com>
2022-08-13 12:27:23 -0500

    init

---
+++ better-lucky-catch-icon.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         🐭️ MouseHunt - Better Lucky Catch Icon
+// @version      1.0.0
+// @description  Upscales the Lucky Catch icon so it isn't so blurry.
+// @license      MIT
+// @author       bradp
+// @namespace    bradp
+// @match        https://www.mousehuntgame.com/*
+// @icon         https://brrad.com/mouse.png
+// @grant        none
+// @run-at      document-end
+// ==/UserScript==
+
+(() => {
+	const style = document.createElement('style');
+	style.innerHTML = `.journal .entry.luckycatchsuccess .journalimage:after {
+		background: url('https://www.mousehuntgame.com/images/ui/camp/trap/stat_luck.png?asset_cache_version=2');
+		background-size: cover;
+		background-repeat: no-repeat;
+		height: 20px;
+		width: 20px;
+		top: -5px;
+		left: -5px;
+	}
+	.journaltext .lucky:after {
+		background: url('https://www.mousehuntgame.com/images/ui/camp/trap/stat_luck.png?asset_cache_version=2');
+		background-size: cover;
+		width: 20px;
+		height: 20px;
+		background-repeat: no-repeat;
+		position: relative;
+		top: 5px;
+		margin: 0;
+	}`;
+	document.head.appendChild(style);
+})();
